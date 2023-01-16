933b8b830aa53a94444c65765ae02fb0f19c5ded
mahiro72 <mahiro.yoshie@gmail.com>
2023-01-17 02:30:48 +0900

    lint

--- src/main.go
+++ src/main.go
@@ -4,7 +4,7 @@ import "github.com/mahiro72/go-simple-echo-server/presentation/http/router"
 
 func main() {
 	r := router.New(3000)
-	if err := r.Serve();err != nil {
+	if err := r.Serve(); err != nil {
 		panic(err)
 	}
 }
